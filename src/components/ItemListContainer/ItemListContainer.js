3be903ed03c345c090338454ecea2682a2db0808
Dimo <dimo.ruso@gmail.com>
2022-07-02 14:40:04 -0300

    se agrego item count

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import "./ItemListContainer.css"
+import "./ItemListContainer.css";
+import ItemCount from "../ItemCount/ItemCount";
 
 const ItemListContainer = ({greeting}) => {
-    return <div className="container-list">{greeting}</div>;
+    return <div className="container-list">{greeting}<ItemCount stock={5} initial={1} onAdd={()=> alert('agregados al carrito ${n} productos')}/></div>
     }
 
 export default ItemListContainer;
